c678be610de39ef73ac66a36add6c767d2978849
Martin Lagrange <martin@ifeelgoods.com>
2014-07-21 16:22:27 +0200

    Add an option to skip blank changes

--- lib/paper_trail/config.rb
+++ lib/paper_trail/config.rb
@@ -3,12 +3,13 @@ require 'singleton'
 module PaperTrail
   class Config
     include Singleton
-    attr_accessor :enabled, :timestamp_field, :serializer, :version_limit
+    attr_accessor :enabled, :timestamp_field, :serializer, :version_limit, :skip_blank_changes
 
     def initialize
       @enabled         = true # Indicates whether PaperTrail is on or off.
       @timestamp_field = :created_at
       @serializer      = PaperTrail::Serializers::YAML
+      @skip_blank_changes = false
     end
   end
 end
